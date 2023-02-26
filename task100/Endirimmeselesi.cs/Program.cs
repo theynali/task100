dcade462f47e766dfaa1e41998415597fb1bc9b1
theynali <125945981+theynali@users.noreply.github.com>
2023-02-26 22:53:24 +0400

    cs file added

---
+++ task100/Endirimmeselesi.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Endirimmeselesi.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double mebleq = 5000;
+            if (mebleq <= 100 && mebleq >0) 
+            {
+                Console.WriteLine(mebleq - (mebleq * 5 / 100));
+            }
+            else if (mebleq > 100 && mebleq <1000)
+            {
+                Console.WriteLine(mebleq - (mebleq * 8 / 100));
+            }
+            else if (mebleq > 1000 )
+            {
+                Console.WriteLine(mebleq - (mebleq * 10 / 100));
+            }
+            else
+            {
+                Console.WriteLine("Duzgun eded daxil edin: ");
+            }
+            
+           
+          
+
+
+
+
+        }
+    }
+}
